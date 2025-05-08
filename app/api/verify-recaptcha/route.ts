d618aa7d7ca7650bed4b9f934b6ff10493a8b7da
Swapna-sp <swapna@farefirst.com>
2025-05-08 17:16:32 +0530

    added recaptcha secret key

--- app/api/verify-recaptcha/route.ts
+++ app/api/verify-recaptcha/route.ts
@@ -59,7 +59,7 @@ export async function POST(request: Request) {
     return NextResponse.json({ success: false, message: 'Token is missing' }, { status: 400 });
   }
 
-  const secret = process.env.NEXT_RECAPTCHA_SECRET_KEY;
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
 
   const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
     method: 'POST',
