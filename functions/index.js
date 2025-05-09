da9bb289b759ed4ef707c9bbd32fdf7bff8b5caf
Swapna-sp <swapna@farefirst.com>
2025-05-09 10:15:10 +0530

    posting phone number to database

--- functions/index.js
+++ functions/index.js
@@ -20,7 +20,7 @@ exports.submitLead = functions.https.onRequest((req, res) => {
     }
 
     try {
-      const { type, search, travelDate, pax, rooms, email, children } = req.body;
+      const { type, search, travelDate, pax, rooms, email, children, phoneNumber } = req.body;
 
 
       await db.collection("travelSearches").add({
@@ -31,7 +31,7 @@ exports.submitLead = functions.https.onRequest((req, res) => {
         children,
         rooms,
         email,
-        phone,
+        phoneNumber,
         timestamp: admin.firestore.FieldValue.serverTimestamp(),
       });
 
